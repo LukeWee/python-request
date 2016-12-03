849b8782b8bccfbcc23313714efb873085689545
Luke Wee <Luke Wee>
2016-12-03 17:08:10 +0800

    Updated

--- request.py
+++ request.py
@@ -1,15 +1,15 @@
 
 
-import requests;
+import requests
 
-url=('http://www.thestar.com.my/','http://www.ebay.com/','https://www.facebook.com/','https://www.youtube.com/','https://www.netflix.com/','http://www.chinapress.com.my/','http://www.yahoo.com/','http://www.google.com/','https://www.bing.com/','https://www.lelong.com.my/');
+url=('http://www.thestar.com.my/','http://www.ebay.com/','https://www.facebook.com/','https://www.youtube.com/','https://www.netflix.com/','http://www.chinapress.com.my/','http://www.yahoo.com/','http://www.google.com/','https://www.bing.com/','https://www.lelong.com.my/')
 
 for count in range(0,9):
-r = requests.get(url[count], auth=('user', 'pass'));
+    r = requests.get(url[count], auth=('user', 'pass'))
 
-print (r.status_code);
-print (r.headers['content-type']);
-print (r.encoding);
-print (r.text);
-print (r.json());
+    print (r.status_code)
+    print (r.headers['content-type'])
+    print (r.encoding)
+    print (r.text)
+    print (r.json())
 
